590eb7133f7979f4b9ad2108401593fc2b7120cd
Adam <adam.cyrille@hotmail.fr>
2018-12-11 14:29:21 +0100

    php

--- vues/Admin.php
+++ vues/Admin.php
@@ -157,7 +157,46 @@
 
 <div id="listuser">
 <br> 
+<?php 
+/**
+* Vue : liste des utilisateurs inscrits
+*/
+?>
 
+<p><?php echo $entete; ?></p>
+
+<table>
+	<thead>
+		<tr>
+
+			<th>Nom</th>
+			<th>Mot de passe crypté</th>
+		</tr>
+	</thead>
+	<tbody>	
+	
+    <?php foreach ($liste as $element) { ?>
+    
+        <tr>
+        		<td>
+				<?php echo $element['AdresseMail']; ?>
+            	</td>
+        		<td>
+        			<?php echo $element['password']; ?>
+        		</td>
+        	</tr>
+    
+    <?php } ?>
+
+	</tbody>
+</table>
+
+
+<?php if(isset($alerte)) { echo AfficheAlerte($alerte);} ?>
+
+<p>
+	<a href="index.php">Retour</a>
+</p>
 </div>
 </div>
 </div>
