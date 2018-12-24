1131318309b696af5f147faf95ebb62c5b4b4ed9
harrati98 <harrati98@gmail.com>
2018-12-24 19:03:34 +0100

    DEL ok

--- controleurs/suppr_maison.php
+++ controleurs/suppr_maison.php
@@ -1,7 +1,6 @@
 <?php 
 					$mysqli = mysqli_connect("localhost", "root", "root", "mydoms","8889");
-					$table = "habitation";
-					$idHabitation=$_POST['idHabitation'];
-					$del = mysqli_query($mysqli,"DELETE FROM habitation WHERE idHabitation='.$idHabitation.'");
+					$idHabitation= $_POST['idHabitation']; 
+					 mysqli_query($mysqli,"DELETE FROM habitation WHERE idHabitation='".$idHabitation."'");
 					header('Location: ../index.php?cible=utilisateurs&fonction=Configuration'); 
-					?>
+?>
