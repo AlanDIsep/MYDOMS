df9aea05de4a44d42c7178ba4f58a847d966a6f9
harrati98 <harrati98@gmail.com>
2019-01-06 17:55:39 +0100

    consigne ok

--- vues/Temperature.php
+++ vues/Temperature.php
@@ -53,8 +53,10 @@ else {
 	
 	
 	<?php $mysqli = mysqli_connect("localhost", "root", "root", "mydoms","8889"); ?>
+
+
 	
-	
+	<!------------------------------------BOUTONS------------------------------------------------------------>
 	<div class="container">
         <button class="toggle">
 		<?php 
@@ -85,25 +87,39 @@ else {
 					?>
 		</button>
     </div>
+	<!------------------------------------------------------------------------------------------------------------>
     
-    <div class="space"></div>
+	<div class="space"></div>
     
 	
     <div id="conteneur">
+
+	
 	<div id="target">
 		<h2>
 		<?php 
-					$mysqli = mysqli_connect("localhost", "root", "root", "mydoms","8889");
+					
 					$nom = mysqli_query($mysqli,"SELECT Nom FROM equipement where Piece_id='1'");
 					$row = mysqli_fetch_assoc($nom);
 					echo $row['Nom'];
 					?>
 		</h2>
-        <form method="post" action="">
+        <?php
+		
+		$etat = mysqli_query($mysqli,"SELECT Etat FROM equipement where idEquipement='5'");
+					$row = mysqli_fetch_assoc($etat);
+				
+					
+		?>
+		
+		<!--------------------------------On/OFF-->
+		<form method="post">
 		<label class="switch">
         <input type="checkbox" name="switch"
+		
+		
 		<?php
-		if(isset($_POST['switch'])) {
+		if(isset($_POST['switch']) and $row='1') {
 		echo 'checked="checked"';
 		} else { 
 		}
@@ -113,11 +129,32 @@ else {
         </label>
 		<?php
 		
+		
+		
+		
+		
+		/*if(isset($_POST['switch']) and $row=1) {
+		$SQL = "UPDATE equipement SET Etat='1' where idEquipement='5'";
+		mysqli_query($mysqli,$SQL);
+		} else if(!isset($_POST['switch']) and $row=1) { 
+		$SQL = "UPDATE equipement SET Etat='0' where idEquipement='5'";
+		mysqli_query($mysqli,$SQL);
+		} else if(isset($_POST['switch']) and $row=0) { 
+		$SQL = "UPDATE equipement SET Etat='1' where idEquipement='5'";
+		mysqli_query($mysqli,$SQL);
+		} else if(!isset($_POST['switch']) and $row=0) { 
+		$SQL = "UPDATE equipement SET Etat='0' where idEquipement='5'";
+		mysqli_query($mysqli,$SQL);
+		}*/
+		
+
 		$switch = (isset($_POST['switch'])) ? 1 : 0;
 		$SQL = "UPDATE equipement SET Etat='.$switch.' where idEquipement='5'";
-		$result = mysqli_query($mysqli,$SQL);
+		mysqli_query($mysqli,$SQL);
 		?>
-		<input type="submit" name="submit" value="appliquer"/>
+		<input type="submit" value="submit"/>
+
+		<!--FIN ON/OFF---------------------------------------------------------------------------->
 		
 		</form>
 		<img src="../CSS/icons/temp.jpg" class="lumi"/></br>
@@ -131,19 +168,31 @@ else {
 			
 			
 			<div id="modulator">
-			<button type="button" onClick="onClick2()" style="margin-left:15px;">-</button>
-			<p class="color-t">Température désirée: <a id="clicks"></a>°</p>
-			<button type="button" onClick="onClick()">+</button>
-			</div>
 			
-			<form method="post">
-			<input type="submit" name="submit" value="submit"/>
-			</form>
+			<p class="color-t">Température désirée: <a id="clicks"></a><output type="text" id="textInput" value="">19°</p>
+			
+			</div>
+			<!--CONSIGNE A DONNER TEMPERATURE ---------------------------------->
+			<form method="post" action="">
 			<?php
-			if(isset($_POST['submit']))
+		
+			?>
+			
+			 <input type="range" name="range" id="range" min="15" max="30" step="1" value="19" onchange="updateTextInput(this.value);" />
+			
+			</br>
+			<input type="submit" value="submit"/>
+			</form>
+			<script>
+			function updateTextInput(val) {
+			document.getElementById('textInput').value=val+'°'; 
+			 }</script>
+			<?php
+			$temp= $_POST['range'];
+			if(isset($_POST['range']))
 			{
-				 $SQL = "UPDATE equipement SET consigne='.$clicks.' where idEquipement=1";
-				 $result = mysqli_query($mysqli,$SQL);
+				 $SQL = "UPDATE equipement SET consigne='.$temp.' where idEquipement='5'";
+				  mysqli_query($mysqli,$SQL);
 			}
 			?>
 
@@ -161,7 +210,7 @@ else {
 		<label class="switch">
         <input type="checkbox" name="switch2"
 		<?php
-		if(isset($_POST['switch2'])) {
+		if(isset($_POST['switch2']) and $row2='1') {
 		echo 'checked="checked"';
 		} else { 
 		}
@@ -171,11 +220,36 @@ else {
         </label>
 		<?php
 		
+		
+		$etat = mysqli_query($mysqli,"SELECT Etat FROM equipement where idEquipement='14'");
+					$row2 = mysqli_fetch_assoc($etat);
+					
+		
+		
+		
+		/*if(isset($_POST['switch2']) and $row=1) {
+		$SQL = "UPDATE equipement SET Etat='1' where idEquipement='14'";
+		mysqli_query($mysqli,$SQL);
+		} else if(!isset($_POST['switch2']) and $row=1) { 
+		$SQL = "UPDATE equipement SET Etat='0' where idEquipement='14'";
+		mysqli_query($mysqli,$SQL);
+		} else if(isset($_POST['switch2']) and $row=0) { 
+		$SQL = "UPDATE equipement SET Etat='1' where idEquipement='14'";
+		mysqli_query($mysqli,$SQL);
+		} else if(!isset($_POST['switch2']) and $row=0) { 
+		$SQL = "UPDATE equipement SET Etat='0' where idEquipement='14'";
+		mysqli_query($mysqli,$SQL);
+		}*/
+		
+
 		$switch2 = (isset($_POST['switch2'])) ? 1 : 0;
-		$SQL = "UPDATE equipement SET Etat='.$switch2.' where idEquipement='14'";
-		$result = mysqli_query($mysqli,$SQL);
+
+
+		$SQL1 = "UPDATE equipement SET Etat='.$switch2.' where idEquipement='14'";
+		mysqli_query($mysqli,$SQL1);
 		?>
-		<input type="submit" name="submit2" value="appliquer"/>
+		
+		<input type="submit" value="submit"/>
 		
 		</form>
 		<img src="../CSS/icons/temp.jpg" class="lumi"/></br>
@@ -197,11 +271,13 @@ else {
 					$row = mysqli_fetch_assoc($nom);
 					echo $row['Nom'];
 					?></h2>
+
+
          <form method="post" action="">
 		<label class="switch">
         <input type="checkbox" name="switch3"
 		<?php
-		if(isset($_POST['switch3'])) {
+		if(isset($_POST['switch3']) and $row='1') {
 		echo 'checked="checked"';
 		} else { 
 		}
@@ -209,13 +285,22 @@ else {
 		>
         <span class="slider round"></span>
         </label>
+		
 		<?php
+			
+
+			
+		$etat = mysqli_query($mysqli,"SELECT Etat FROM equipement where idEquipement='16'");
+					$row = mysqli_fetch_assoc($etat);
+					
+		
+
 		
 		$switch3 = (isset($_POST['switch3'])) ? 1 : 0;
-		$SQL = "UPDATE equipement SET Etat='.$switch3.' where idEquipement='16'";
-		$result = mysqli_query($mysqli,$SQL);
+		$SQL2 = "UPDATE equipement SET Etat='.$switch3.' where idEquipement='16'";
+		mysqli_query($mysqli,$SQL2);
 		?>
-		<input type="submit" name="submit3" value="appliquer"/>
+		<input type="submit" name="submit" value="submit"/>
 		
 		</form>
         <img src="../CSS/icons/temp.jpg" class="lumi"/>
@@ -318,7 +403,7 @@ $('.toggle3').click(function()
         if (clicks < 35) {
 		<?php $clicks += 1; ?>
 		clicks += 1;
-        document.getElementById("clicks").innerHTML = <?php echo $clicks2; ?>;
+        document.getElementById("clicks").innerHTML = clicks;
 	}};
     function onClick2() {
         if (clicks > 13) {
