5664a538cb3b16a7721ac9e6fb923704518fe0d8
Adam <adam.cyrille@hotmail.fr>
2019-06-07 10:21:13 +0200

    dede

--- vues/Contact.php
+++ vues/Contact.php
@@ -123,22 +123,14 @@ else {
             <input type="date" id="datepanne" name="Date" required>
             <br><br>
             <?php
-$ch = curl_init();
-curl_setopt(
-$ch,
-CURLOPT_URL,
-"http://projets-tomcat.isep.fr:8080/appService?ACTION=GETLOG&TEAM=9999");
-curl_setopt($ch, CURLOPT_HEADER, FALSE);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-$data = curl_exec($ch);
-curl_close($ch);
-echo "Raw Data:<br />";
-echo("$data");
-$data_tab = str_split($data,33);
-echo "Tabular Data:<br />";
-for($i=0, $size=count($data_tab); $i<$size; $i++){
-echo "Trame $i: $data_tab[$i]<br />";
-}
+  $url = "http://projets-tomcat.isep.fr:8080/appService?ACTION=GETLOG&TEAM=9999";
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, $url);
+  curl_setopt($ch, CURLOPT_HEADER, FALSE);
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+  $data = curl_exec($ch);
+  echo($data);
+  curl_close($ch);
 
 ?>
 <br>
