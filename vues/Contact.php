140f6dd51bd371e267993cd77498568120d96346
Adam <adam.cyrille@hotmail.fr>
2019-06-07 11:34:10 +0200

    dede

--- vues/Contact.php
+++ vues/Contact.php
@@ -133,31 +133,47 @@ $capteur_type = array("1" => "distance modèle 1", "2" => "distance modèle 2", 
 "h" => "Requête Heure 2", "p" => "Requête data", "q" => "Requête année");
 $requete_tyep = array("1" => "Requête en écriture", "2" =>"Requête en lecture", "3" => "Requête en lecture/écriture");
 
-  $url = "http://projets-tomcat.isep.fr:8080/appService?ACTION=GETLOG&TEAM=9999";
+  $url = "http://projets-tomcat.isep.fr:8080/appService/?ACTION=GETLOG&TEAM=001A";
   $ch = curl_init();
   curl_setopt($ch, CURLOPT_URL, $url);
   curl_setopt($ch, CURLOPT_HEADER, FALSE);
   curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-  $data = curl_exec($ch);
+  $data= (curl_exec($ch));
   echo "Bonjour";
-  echo $data;
+  
   curl_close($ch);
+  echo $data;
   $data_tab = str_split($data,33);
-  if ($type_capteur == "Capteur de Luminosité") {
-    $type_capteur_num = "5";
-  } else if ($type_capteur == "Capteur de distance") {
-    $type_capteur_num = "1";
-  }
-  $res = "0";
-  for($i=0, $size=count($data_tab);$i<$size;$i++){
-    $trame = $data_tab[$i];
-    //echo "$trame <br/>";
-    list($t,$o,$r,$c,$n,$v,$a,$x,$year,$month,$day,$hour,$min,$sec) = sscanf($trame,"%1s%4s%1s%1s%2s%4s%4s%2s%4s%2s%2s%2s%2s%2s");
-    list($t,$o,$r,$c,$n,$v,$a,$x,$year,$month,$day,$hour,$min,$sec) = sscanf($trame,"%1d%4s%1s%1s%2x%4s%4s%2s%4d%2d%2d%2d%2d%2d");
-    if ($c == $type_capteur_num) {
-      $res = (int)$v;
-    }
-  }
+echo "Tabular Data:<br />";
+for($i=0, $size=count($data_tab); $i<$size; $i++){
+echo "Trame $i: $data_tab[$i]<br />";
+}
+$trame = $data_tab[0];
+// décodage avec des substring
+$t = substr($trame,0,1);
+$o = substr($trame,1,4);
+// …
+// décodage avec sscanf
+list($t, $o, $r, $c, $n, $v, $a, $x, $year, $month, $day, $hour, $min, $sec) =
+sscanf($trame,"%1s%4s%1s%1s%2s%4s%4s%2s%4s%2s%2s%2s%2s%2s");
+echo("<br />$t,$o,$r,$c,$n,$v,$a,$x,$year,$month,$day,$hour,$min,$sec<br />");
+echo"la valeur de la température est $v"
+  // $data_tab = str_split($data,33);
+  // if ($type_capteur == "Capteur de Luminosité") {
+  //   $type_capteur_num = "5";
+  // } else if ($type_capteur == "Capteur de distance") {
+  //   $type_capteur_num = "1";
+  // }
+  // $res = "0";
+  // for($i=0, $size=count($data_tab);$i<$size;$i++){
+  //   $trame = $data_tab[$i];
+  //   //echo "$trame <br/>";
+  //   list($t,$o,$r,$c,$n,$v,$a,$x,$year,$month,$day,$hour,$min,$sec) = sscanf($trame,"%1s%4s%1s%1s%2s%4s%4s%2s%4s%2s%2s%2s%2s%2s");
+  //   list($t,$o,$r,$c,$n,$v,$a,$x,$year,$month,$day,$hour,$min,$sec) = sscanf($trame,"%1d%4s%1s%1s%2x%4s%4s%2s%4d%2d%2d%2d%2d%2d");
+  //   if ($c == $type_capteur_num) {
+  //     $res = (int)$v;
+  //   }
+  // }
  ?>
 
 
